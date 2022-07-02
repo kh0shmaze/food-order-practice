f70c92abdca3c06c9cdb0ab76d1b5ebfcb609c71
unknown <kh0shmaze@gmail.com>
2022-07-02 22:35:09 +0430

    start context!!

---
+++ src/store/CartProvider.js
@@ -0,0 +1,22 @@
+import CartContext from "./cart-context";
+
+const CartPtovider = (props) => {
+  const addItemToCartHandler = (item) => {};
+
+  const removeItemFromCartHandler = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+
+  return (
+    <CartContext.Provider vlaue={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartPtovider;
